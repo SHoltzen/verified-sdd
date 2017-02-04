2e61068ac70e5468db4dee48c70e6532924c2d12
Steven Holtzen <sholtzen@gmail.com>
2017-02-03 16:01:09 -0800

    sdd inference rules

---
+++ coq/IndSdd.v
@@ -0,0 +1,84 @@
+Require Export Bool.Bool.
+Require Export List.
+Print Bool.
+
+Notation "x :: l" := (cons x l)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+Notation "x ++ y" := (app x y)
+                     (right associativity, at level 60).
+
+Inductive vtree : Type :=
+| VNode : vtree -> vtree -> vtree
+| VAtom : nat -> vtree.
+
+Inductive atom : Type :=
+| AFalse : atom
+| ATrue : atom
+| AVar :  nat -> bool -> atom.
+
+Inductive sdd : Type :=
+          (*prime sub   next *)
+| Or: list (sdd * sdd) -> sdd
+| Atom : atom -> sdd.
+
+Inductive op : Type :=
+| OAnd.
+
+Inductive test : list (bool*bool) -> Prop :=
+| EmptyList : test nil
+| OnlyTrue : test [(true, true)]
+| HeadFlip : forall (b1 b2 : bool) (l : list (bool*bool)), 
+    test((b1, b2)::l) -> test([(b2, b1)]).
+
+
+Inductive sdd_apply : op -> sdd -> sdd -> sdd -> Prop :=
+| AtomAndTT : sdd_apply OAnd (Atom ATrue) (Atom ATrue) (Atom ATrue)
+| AtomAndTF : sdd_apply OAnd (Atom ATrue) (Atom AFalse) (Atom AFalse) 
+| AtomAndFT : sdd_apply OAnd (Atom AFalse) (Atom ATrue) (Atom AFalse)
+| AtomAndFF : sdd_apply OAnd (Atom AFalse) (Atom AFalse) (Atom AFalse)
+| AtomAndVT : forall (n : nat) (b : bool), sdd_apply OAnd (Atom (AVar n b)) (Atom ATrue) (Atom (AVar n b))
+| AtomAndTV : forall (n : nat) (b : bool), sdd_apply OAnd (Atom ATrue) (Atom (AVar n b)) (Atom (AVar n b))
+| AtomAndVF : forall (n : nat) (b : bool), sdd_apply OAnd (Atom AFalse) (Atom (AVar n b)) (Atom AFalse)
+| AtomAndFV : forall (n : nat) (b : bool), sdd_apply OAnd (Atom (AVar n b)) (Atom AFalse) (Atom AFalse)
+| AtomAndVVEq : forall (n : nat) (b : bool), sdd_apply OAnd (Atom (AVar n b)) (Atom (AVar n b)) (Atom (AVar n b))
+| AtomAndVVNEq : forall (n : nat) (b1 : bool) (b2 : bool),
+    (b1 <> b2) -> sdd_apply OAnd (Atom (AVar n b1)) (Atom (AVar n b2)) (Atom AFalse)
+| ApplyOr : forall (l1 l2 res: list (sdd*sdd)) (o : op),
+    or_list o l1 l2 res -> sdd_apply o (Or l1) (Or l2) (Or res)
+with
+(* an auxiliary inductive type to handle applying together two lists of primes and subs *)
+or_list : op -> list (sdd*sdd) -> list (sdd*sdd) -> list(sdd*sdd) -> Prop :=
+| EmptyLeft : forall (l : list (sdd * sdd)) (o : op),
+    or_list o [] l []
+| Single : forall (prime sub : sdd) (ltail singleres orres linput : list (sdd * sdd)) (o : op),
+    single_list o prime sub linput singleres ->
+    or_list o ltail linput orres ->
+    or_list o ((prime, sub)::ltail) linput (singleres ++ orres)
+with
+(* an auxiliary inductive type to handle applying together a single prime and sub with a list of primes
+   and subs *)
+single_list : op -> sdd -> sdd -> list (sdd * sdd) -> list (sdd * sdd) -> Prop :=
+| EmptyRight : forall (prime sub : sdd) (o : op),
+    single_list o prime sub [] []
+| NonEmptyRight : forall (prime1 prime2 sub1 sub2 newprime newsub : sdd) (o : op) (tl subres : list (sdd * sdd)),
+    sdd_apply OAnd prime1 prime2 newprime ->
+    sdd_apply o sub1 sub2 newsub ->
+    single_list o prime1 sub1 tl subres -> (* process the rest of the list *)
+    single_list o prime1 sub1 ((prime2, sub2)::tl) ((newprime, newsub)::subres).
+
+Example ex_sdd_apply1:
+  sdd_apply OAnd (Atom ATrue) (Atom AFalse) (Atom AFalse).
+Proof.
+  apply AtomAndTF. Qed.
+
+(* this gon' be fun... *)
+(* Theorem self_apply: *)
+(*   forall (sdd : sdd), sdd_apply OAnd sdd sdd sdd. *)
+(* Proof. *)
+(*   intros. induction sdd0. *)
+(*   - induction l. *)
+(*     * apply (ApplyOr [] [] []). apply EmptyLeft. *)
+(*     * apply ApplyOr.  destruct a. remember ([(s,s0)]++l) as lb. *)
+(*       apply (Single s s0 l s s0 l). *)
--- coq/SDD.v
+++ coq/SDD.v
@@ -8,87 +8,40 @@ Inductive vtree : Type :=
 | VNode : vtree -> vtree -> vtree
 | VAtom : nat -> vtree.
 
-Inductive atom : nat -> Type :=
-| AFalse : forall n, atom n
-| ATrue : forall n, atom n
-| AVar : forall n, bool -> atom n.
+Inductive atom :  Type :=
+| AFalse : atom
+| ATrue : atom
+| AVar :  nat -> bool -> atom.
 
-Inductive sdd : vtree -> Type :=
-| Or : forall l r : vtree, list (sdd l * sdd r) -> sdd (VNode l r)
-| Atom : forall n, atom n -> sdd (VAtom n).
+Inductive sdd : Type :=
+| Or : list (sdd * sdd) -> sdd
+| Atom : atom -> sdd.
 
 Inductive ops : Type :=
 | OAnd : ops
 | OOr : ops.
 
 
-(* Inductive atom_eq_rel : atom -> atom -> Prop := *)
-(* | EqTrue : atom_eq_rel ATrue ATrue *)
-(* | EqFalse : atom_eq_rel AFalse AFalse *)
-(* | EqVar : forall n, atom_eq_rel (AVar n) (AVar n). *)
-
-(* Fixpoint atom_eq n1 (a : atom n1) n2 (b : atom n2): bool := *)
-(*   match a, b with *)
-(*   | ATrue n1, ATrue n2 => true *)
-(*   | AFalse n1, AFalse n2 => true *)
-(*   | AVar n1, AVar n2 => beq_nat n1 n2 *)
-(*   | _, _ => false *)
-(*   end. *)
-
-
-
-(* Fixpoint sdd_eq (s1: sdd) (s2 : sdd) : bool := *)
-(*   match s1, s2 with *)
-(*   | Or [], Or [] => true *)
-(*   | Or (prime1, sub1) :: rst1, Or (prime2, sub2) :: rst2 => *)
-(*     sdd_eq  *)
-(*   | Atom a1, Atom a2 => atom_eq a1 a2 *)
-(*   | _, _ => false *)
-(*   end. *)
-
-
-
-(* let rec expanded_sdd vtree term = *)
-(*   match vtree with *)
-(*   | VNode(l, r) -> *)
-(*     let el, er = if term = True then True, True else True, False in *)
-(*     let l = expanded_sdd l el and r = expanded_sdd r er in *)
-(*     Or([l, r]) *)
-(*   | VAtom(a) -> Atom(term) *)
-
-(* (** generate an SDD given an atom from the Boolean expression grammar *) *)
-(* let rec sdd_of_atom vtree atom v = *)
-(*   match vtree with *)
-(*   | VAtom(i) -> if i = atom then *)
-(*       Atom(Var(i, v)) *)
-(*     else Atom(True) *)
-(*   | VNode(vl, vr) -> *)
-(*     let left_t = sdd_of_atom vl atom v and *)
-(*     right_t = sdd_of_atom vr atom v and *)
-(*     left_f = sdd_of_atom vl atom (not v) in *)
-(*     let f = expanded_sdd vr False in *)
-(*     Or([(left_t, right_t); (left_f, f)]) (* if you just do it this way, you don't need to expand *) *)
-
-Fixpoint expanded_sdd (v : vtree) (term : bool) : sdd v :=
+Fixpoint expanded_sdd (v : vtree) (term : bool) : sdd :=
   match v with
-  | VAtom i => if term then Atom i (ATrue i) else Atom i (AFalse i)
+  | VAtom i => if term then Atom ATrue else Atom AFalse
   | VNode l r =>
     let (el, er) := if term then (true, true) else (true, false) in
     let lsdd := expanded_sdd l el in
     let rsdd := expanded_sdd r er in
-    Or l r ((lsdd, rsdd) :: nil)
+    Or ((lsdd, rsdd) :: nil)
   end.
 
 
-Fixpoint sdd_of_atom (v : vtree) (a : nat) (value : bool) : sdd v :=
+Fixpoint sdd_of_atom (v : vtree) (a : nat) (value : bool) : sdd :=
   match v with
-  | VAtom n => if beq_nat a n then Atom n (AVar n value) else Atom n (ATrue n)
+  | VAtom n => if beq_nat a n then Atom (AVar n value) else Atom n (ATrue n)
   | VNode vl vr =>
     let left_t := sdd_of_atom vl a value in
     let left_f := sdd_of_atom vl a (negb value) in
     let right_t := sdd_of_atom vr a value in
     let right_f := expanded_sdd vr false in
-    Or vl vr ((left_t, right_t) :: (left_f, right_f) :: nil)
+    Or ((left_t, right_t) :: (left_f, right_f) :: nil)
   end.
 
 Definition atom_and (n : nat) (a : atom n) (b : atom n) : atom n :=
@@ -116,21 +69,19 @@ Definition apply_op (op : ops) (n : nat) (a : atom n) (b : atom n) :=
 
 
 Fixpoint apply (o : ops) (v : vtree) (a : sdd v) (b : sdd v) : sdd v :=
-  match a, b with
-  | Atom n a1, Atom _ a2 => apply_op o n a1 a2
-  | Or lvtree rvtree l1, Or _ _ l2 =>
-    let new_or_lst :=
-        List.fold_left l1 nil
-                       (fun a outer_accum =>
-                          let (prime1, sub1) := a in
-                          List.fold_left l2 outer_accum (fun b inner_accum =>
-                                                           let (prime2, sub2) := b in
-                                                           let new_prime := apply OAnd lvtree prime1 prime2 in
-                                                           let new_sub := apply o rvtree sub1 sub2 in
-                                                           (new_prime, new_sub) :: inner_accum
-                       )) in
-    Or (VNode lvtree rvtree) new_or_lst
-
-
+  match v, a, b with
+  | VAtom n, Atom _ a1, Atom _ a2 => apply_op o n a1 a2
+  | VNode vl vr, Or _ _ l1, Or _ _ l2 =>
+    Or (process_or_list o vl vr l1 l2)
+  end with
+process_or_list (o : ops) (vl : vtree) (vr : vtree) (l1 : list (sdd vl * sdd vr)) (l2 : list (sdd vl * sdd vr)) : list (sdd vl * sdd vr) :=
+  match l1 with
+  | nil => nil
+  | x::xs =>
+    (process_or_single o vl vr x l2) :: (process_or_list o vl vr xs l2)
+  end with
+process_or_single (o : ops) (vl : vtree) (vr : vtree) (cur : (sdd vl * sdd vr)) (l2 : list (sdd vl * sdd vr)) : list (sdd vl * sdd vr) :=
+  match l2 with
+  | nil => nil
+  | (sdd_a _, sdd_b _)::rst => nil
   end.
-
